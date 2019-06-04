794eaec7bd159ce282824788b94fc19dec3a3851
Aashima25 <mvere@allstate.com>
2019-06-04 11:23:34 +0530

    implemented routing

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AboutPage = () => (
   <div>
     <h1>We are in about page</h1>
+    <Link to="/">back</Link>
   </div>
 );
 
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const HomePage = () => (
   <div>
     <h1>We are in home page</h1>
-    {/* <Link to="AboutPage">learn more</Link> */}
+    <Link to="aboutus">learn more</Link>
   </div>
 );
 
